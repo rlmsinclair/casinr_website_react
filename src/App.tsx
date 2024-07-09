c86b6e0433387838b7cd73732a2008a81c59b344
rlmsinclair <rlmsinclair@gmail.com>
2024-07-10 00:25:01 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Connection, PublicKey, Transaction, SystemProgram } from '@solana/web3.js';
 import { Buffer } from 'buffer';
 
